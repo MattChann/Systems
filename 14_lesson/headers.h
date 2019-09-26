681614936f538df4ca78a3f5e158b607b22f71c3
Matt <mchan00@stuy.edu>
2019-09-26 11:46:38 -0400

    Pair work

---
+++ 14_lesson/headers.h
@@ -0,0 +1,5 @@
+int strlen(char *s);
+char *strncpy(char *dest, char *source, int n);
+char *strcat(char *dest, char *source);
+int strcmp(char *s1, char *s2);
+char * strchr(char *s, char c);
---
+++ 14_lesson/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+	char s1[10] = "Morty";
+	char s2[20] = "Rick";
+	char s3[12] = "Hello World";
+	
+	printf("s1: [%s]\n", s1);
+	printf("s2: [%s]\n", s2);
+	printf("s3: [%s]\n", s3);
+
+}
