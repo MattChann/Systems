02b5c0eae0f81c95406b3de7a28e289dd785e630
Matthew Chan <mchan00@stuy.edu>
2019-10-17 01:36:36 -0400

    Finished assignment

--- definitions.c
+++ definitions.c
@@ -1,3 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+#include "headers.h"
 
+struct StuyStudent exampleStudent() {
+    char *exampleNames[] = {"Kazi", "Michael", "Eric", "Ray", "Ace Ventura", "Shaggy", "Jo"};
+    
+    struct StuyStudent student;
+    struct StuyStudent *pStudent = &student;
+
+    srand(time(NULL));
+    pStudent -> name = exampleNames[rand() % 6];
+    pStudent -> mentalStability = (rand() % 101);
+    pStudent -> visitsToFerrys = (rand() % 1000001);
+    return student;
+}
+
+void structPrint(struct StuyStudent *student) {
+    printf("|%s\n", student -> name);
+    printf("|-------\n");
+    printf("|Mental Stability Level: %d/100\n", student -> mentalStability);
+    printf("|Amount of Visits to Ferry's: %d\n", student -> visitsToFerrys);
+}
+
+void changeStudent(struct StuyStudent *student, char *newName, int newStability, int newVisitAmount) {
+    student -> name = newName;
+    student -> mentalStability = newStability;
+    student -> visitsToFerrys = newVisitAmount;
+}
--- main.c
+++ main.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include "headers.h"
 
 int main() {
+    printf("=======Testing Generating and Printing Example Student=======\n");
+    struct StuyStudent student = exampleStudent();
+    struct StuyStudent *pStudent = &student;
+    structPrint(pStudent);
+
+    printf("\n=======Testing Changing Values of Example Student=======\n");
+    printf("Expecting: Rick, -1/100, and -1...\n");
+    changeStudent(pStudent, "Rick", -1, -1); // since the generated values can never be negative we know if we really changed the values
+    structPrint(pStudent);
+
     return 0;
 }
