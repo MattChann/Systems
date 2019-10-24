6ddebf7aa82fd3a2e8a1d63c75b7b32c4250c7e8
Matt <mchan00@stuy.edu>
2019-10-24 00:50:59 -0400

    Finished tests

--- main.c
+++ main.c
@@ -3,5 +3,25 @@
 #include "headers.h"
 
 int main() {
+    struct node *testNode;
+    
+    printf("Printing Empty List:\n");
+    print_list(testNode);
+    
+    printf("\nAdding #s 0-9 to list\n");
+    int i;
+    for(i=0; i<=9; i++) {
+        testNode = insert_front(testNode, i);
+    }
+    
+    printf("Printing List:\n");
+    print_list(testNode);
+
+    printf("\nFreeing List:\n");
+    testNode = free_list(testNode);
+
+    printf("\nPrinting List:\n");
+    print_list(testNode);
+
     return 0;
 }
