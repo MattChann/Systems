582283ae47aaef8017585047fb8e6c7d77a32cc5
Matt <mchan00@stuy.edu>
2019-11-11 14:53:37 -0500

    More work

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include <fcntl.h>
 #include <errno.h>
 #include <unistd.h>
@@ -10,13 +11,13 @@ int genRandInt() {
 		printf("error %d: %s\n", errno, strerror(errno));
 		return 0;
 	}
-    int buffer;
+    int *buffer;
     if (read(fd, buffer, sizeof(int)) < 0) {
         printf("error %d: %s\n", errno, strerror(errno));
 		return 0;
     }
     close(fd);
-    return buffer;
+    return *buffer;
 }
 
 int main() {
@@ -30,27 +31,27 @@ int main() {
         randNums[i] = randInt;
     }
 
-    printf("Writing numbers to file...\n");
+    printf("\nWriting numbers to file...\n");
     int fd = open("output", O_RDWR | O_CREAT);
     if (fd < 0) {
 		printf("error %d: %s\n", errno, strerror(errno));
 		return 0;
 	}
-    if (write(fd, randNums, ARRAY_SIZE * sizeof(int)) < 0) {
+    if (write(fd, randNums, sizeof(randNums)) < 0) {
         printf("error %d: %s\n", errno, strerror(errno));
 		return 0;
     }
 
-    printf("Reading numbers from file...\n");
+    printf("\nReading numbers from file...\n");
     int buffer[ARRAY_SIZE];
-    if (read(fd, buffer, sizeof(int)) < 0) {
+    if (read(fd, buffer, sizeof(buffer)) < 0) {
         printf("error %d: %s\n", errno, strerror(errno));
 		return 0;
     }
 
-    printf("Verification that written values were the same:\n");
+    printf("\nVerification that written values were the same:\n");
     for(i=0; i<ARRAY_SIZE; i++) {
-        printf("\trandom i: %d\n", buffer[i]);
+        printf("\trandom %d: %d\n", i, buffer[i]);
     }
 
 
