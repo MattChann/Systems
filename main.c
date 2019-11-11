4defb75b440809ca938fceab708178ed1303d90a
Matt <mchan00@stuy.edu>
2019-11-11 17:09:17 -0500

    Finished testing and fixing

--- main.c
+++ main.c
@@ -4,20 +4,21 @@
 #include <fcntl.h>
 #include <errno.h>
 #include <unistd.h>
+#include <sys/stat.h>
 
 int genRandInt() {
-    int fd = open("/dev/random", O_RDONLY);
+    int fd = open("/dev/urandom", O_RDONLY);
 	if (fd < 0) {
 		printf("error %d: %s\n", errno, strerror(errno));
 		return 0;
 	}
-    int *buffer;
-    if (read(fd, buffer, sizeof(int)) < 0) {
+    int randomInt;
+    if (read(fd, &randomInt, sizeof(int)) < 0) {
         printf("error %d: %s\n", errno, strerror(errno));
 		return 0;
     }
     close(fd);
-    return *buffer;
+    return randomInt;
 }
 
 int main() {
@@ -31,23 +32,36 @@ int main() {
         randNums[i] = randInt;
     }
 
+
     printf("\nWriting numbers to file...\n");
-    int fd = open("output", O_RDWR | O_CREAT);
+    umask(0);
+    int fd = open("output", O_RDWR | O_CREAT, 0666);
     if (fd < 0) {
 		printf("error %d: %s\n", errno, strerror(errno));
 		return 0;
 	}
-    if (write(fd, randNums, sizeof(randNums)) < 0) {
+    if (write(fd, &randNums, ARRAY_SIZE * sizeof(int)) < 0) {
+        printf("error %d: %s\n", errno, strerror(errno));
+		return 0;
+    }
+    if (close(fd) < 0) {
         printf("error %d: %s\n", errno, strerror(errno));
 		return 0;
     }
 
+
     printf("\nReading numbers from file...\n");
+    int fid = open("output", O_RDONLY);
+    if (fid < 0) {
+		printf("error %d: %s\n", errno, strerror(errno));
+		return 0;
+	}
     int buffer[ARRAY_SIZE];
-    if (read(fd, buffer, sizeof(buffer)) < 0) {
+    if (read(fid, &buffer, ARRAY_SIZE * sizeof(int)) < 0) {
         printf("error %d: %s\n", errno, strerror(errno));
 		return 0;
     }
+
 
     printf("\nVerification that written values were the same:\n");
     for(i=0; i<ARRAY_SIZE; i++) {
@@ -56,6 +70,6 @@ int main() {
 
 
 
-    close(fd);
+    close(fid);
     return 0;
 }
