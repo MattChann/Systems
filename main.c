4c100aa06e90680081d102a754707d43ba5279ed
Matt <mchan00@stuy.edu>
2019-11-18 11:48:23 -0500

    Work

--- main.c
+++ main.c
@@ -6,5 +6,39 @@
 #include <dirent.h>
 
 int main() {
+    DIR *dirStream = opendir(".");
+    if (dirStream == NULL) {
+        printf("error %d: %s\n", errno, strerror(errno));
+        return 0;
+    }
+    printf("Statistics for directory: \n");
+
+    struct dirent *dirEntry;
+    long size = 0;
+    for (dirEntry = readdir(dirStream); dirEntry != NULL; dirEntry = readdir(dirStream)) {
+        size += dirEntry->d_reclen;
+    }
+    rewinddir(dirStream);
+
+    printf("")
+    struct dirent *dirEntry;
+    for (dirEntry = readdir(dirStream); dirEntry != NULL; dirEntry = readdir(dirStream)) {
+        if (dirEntry->d_type == DT_DIR) {
+            
+        }
+    }
+    rewinddir(dirStream);
+
+    struct dirent *dirEntry;
+    for (dirEntry = readdir(dirStream); dirEntry != NULL; dirEntry = readdir(dirStream)) {
+        if (dirEntry->d_type == DT_DIR) {
+            
+        }
+    }
+
+
+
+
+    closedir();
     return 0;
 }
