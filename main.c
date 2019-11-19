b8725bc11c3ac1bafccd9e107452956cbc02b9fd
Matt <mchan00@stuy.edu>
2019-11-18 21:45:43 -0500

    Finished work

--- main.c
+++ main.c
@@ -3,42 +3,60 @@
 #include <sys/stat.h>
 #include <errno.h>
 #include <string.h>
+#include <unistd.h>
+#include <time.h>
 #include <dirent.h>
 
-int main() {
-    DIR *dirStream = opendir(".");
+int main(int argc, char * argv[]) {
+    DIR *dirStream;
+    char dirString[256];
+    if (argc > 1) {
+        strcpy(dirString, argv[1]);
+    }
+    else {
+        printf("Please enter a directory to scan in: ");
+        fgets(dirString, 200, stdin);
+        int i;
+        for (i=0; dirString[i] != '\0'; i++) {
+            if (dirString[i] == '\n') {
+                dirString[i] = '\0';
+            }
+        }
+        printf("\n");
+    }
+    dirStream = opendir(dirString);
+    
     if (dirStream == NULL) {
         printf("error %d: %s\n", errno, strerror(errno));
         return 0;
     }
-    printf("Statistics for directory: \n");
+    printf("Statistics for directory: %s\n", dirString);
 
     struct dirent *dirEntry;
     long size = 0;
     for (dirEntry = readdir(dirStream); dirEntry != NULL; dirEntry = readdir(dirStream)) {
         size += dirEntry->d_reclen;
     }
+    printf("Total Diectory Size: %ld Bytes\n", size);
     rewinddir(dirStream);
 
-    printf("")
-    struct dirent *dirEntry;
+    printf("Directories:\n");
     for (dirEntry = readdir(dirStream); dirEntry != NULL; dirEntry = readdir(dirStream)) {
         if (dirEntry->d_type == DT_DIR) {
-            
+            printf("\t%s\n", dirEntry->d_name);
         }
     }
     rewinddir(dirStream);
 
-    struct dirent *dirEntry;
+    printf("Regular files:\n");
     for (dirEntry = readdir(dirStream); dirEntry != NULL; dirEntry = readdir(dirStream)) {
-        if (dirEntry->d_type == DT_DIR) {
-            
+        if (dirEntry->d_type != DT_DIR) {
+            printf("\t%s\n", dirEntry->d_name);
         }
     }
 
 
 
-
-    closedir();
+    closedir(dirStream);
     return 0;
 }
