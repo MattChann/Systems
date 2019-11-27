c4e35efce3f290dc20f27a6d022ca94102d02242
Matt <mchan00@stuy.edu>
2019-11-26 22:10:47 -0500

    Testing strsep

--- main.c
+++ main.c
@@ -3,6 +3,20 @@
 #include <string.h>
 #include <unistd.h>
 
+char ** parse_args(char * line);
+
+
 int main() {
+    char line[100] = "woah-this-is-cool";
+    char *s1 = line;
+    while (*s1 != '\0') {
+        printf("[%s]\n", strsep( &s1, "-" ));
+        printf("[%s]\n", s1);
+    }
     return 0;
+}
+
+
+char ** parse_args(char * line) {
+    return NULL;
 }
